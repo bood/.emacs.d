76110e586fc6825b07530867be4428a5c6060900
Bood Qian <bood@glowing.com>
2017-06-16 10:24:26 +0800

    Adjust company setting

--- custom.mac.el
+++ custom.mac.el
@@ -39,6 +39,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-dabbrev-downcase nil)
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 2)
  '(default-frame-alist
     (quote
      ((vertical-scroll-bars)
