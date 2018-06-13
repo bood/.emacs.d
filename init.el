101228cff6605dc7afc60bf7445197880e54faf8
Bood Qian <bood@glowing.com>
2018-06-13 11:07:54 +0800

    Ediff: Press d to copy both A and B to buffer C

--- init.el
+++ init.el
@@ -551,6 +551,16 @@
         (define-key map [(shift mouse-2)] 'hs-mouse-toggle-hiding)
         map))
 
+;;;; Pressing d will copy both A and B to buffer C
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
 ;;;; Machine Specific
 (if (string-equal system-type "windows-nt")
     (setq custom-file "~/.emacs.d/custom.win.el")
